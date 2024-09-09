78f9002adce59bb08bf6cfb2d7e8b63d10e7e5c4
wzw <wang.zhiwn@outlook.com>
2024-09-10 01:15:04 +0800

    更新

---
+++ config/webpack.prod.js
@@ -0,0 +1,149 @@
+const path = require("node:path");
+const EslintWebpackPlugin = require("eslint-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+const TerserWebpackPlugin = require("terser-webpack-plugin");
+const ImageMinimizerPlugin = require("image-minimizer-webpack-plugin");
+
+module.exports = {
+    entry: "./src/main.js",
+    output: {
+        path: path.resolve(__dirname, "../dist"),
+        filename: "static/js/[name].[contenthash:10].js",
+        chunkFilename: "static/js/[name].[contenthash:10].chunk.js", // 动态导入的chunk
+        assetModuleFilename: "static/assets/[hash:10][ext][query]",
+        clean: true,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    "css-loader",
+                    "postcss-loader" // 配合postcss.config.js和browserslist
+                ]
+            },
+            {
+                test: /\.less$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    "css-loader",
+                    "postcss-loader",
+                    "less-loader"
+                ]
+            },
+            {
+                test: /\.s[ac]ss$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    "css-loader",
+                    "postcss-loader",
+                    "sass-loader"
+                ]
+            },
+            {
+                test: /\.styl/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    "css-loader",
+                    "postcss-loader",
+                    "stylus-loader"
+                ]
+            },
+            {
+                test: /\.(png|jpe?g|gif|webp|svg)$/,
+                type: "asset",
+                generator: {
+                    filename: "static/images/[hash:10][ext][query]",
+                },
+                parser: {
+                    dataUrlCondition: {
+                        maxSize: 4 * 1024
+                    }
+                }
+            },
+            {
+                test: /\.(woff2|ttf)$/,
+                type: "asset/resource",
+                generator: {
+                    filename: "static/fonts/[hash:10][ext][query]",
+                }
+            },
+            {
+                test: /\.jsx?$/,
+                exclude: /node_modules/,
+                loader: "babel-loader",
+                options: {
+                    cacheDirectory: true,
+                    cacheCompression: false
+                }
+            }
+        ]
+    },
+    plugins: [
+        new EslintWebpackPlugin({
+            configType: "flat",
+            context: path.resolve(__dirname, "../src"),
+            cache: true,
+            cacheLocation: path.resolve(__dirname, "../node_modules/.cache/eslintcache"),
+            // webpack官网中关于eslint-webpack-plugin中提到,需要设置eslintPath为eslint/use-at-your-own-risk
+            eslintPath: "eslint/use-at-your-own-risk"
+        }),
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, "../public/index.html"),
+        }),
+        new MiniCssExtractPlugin({
+            filename: "static/css/[name].[contenthash:10].css",
+            chunkFilename: "static/css/[name].[contenthash:10].chunk.css"
+        })
+    ],
+    mode: "production",
+    devtool: false,
+    optimization: {
+        splitChunks: {
+            chunks: "all"
+        },
+        runtimeChunk: {
+            name: entryPoint => `runtime~${entryPoint.name}`
+        },
+        minimizer: [
+            new CssMinimizerPlugin(),
+            new TerserWebpackPlugin(),
+            new ImageMinimizerPlugin({
+                minimizer: {
+                    implementation: ImageMinimizerPlugin.imageminMinify,
+                    options:{
+                        plugins: [
+                            ["gifsicle", { interlaced: true }],
+                            ["jpegtran", { progressive: true }],
+                            ["optipng", { optimizationLevel: 5 }],
+                            [
+                                "svgo",
+                                {
+                                    plugins: extendDefaultPlugins([
+                                        {
+                                            name: "removeViewBox",
+                                            active: false
+                                        },
+                                        {
+                                            name: "addAttributesToSVGElement",
+                                            params: {
+                                                attributes: [{ xmlns: "http://www.w3.org/2000/svg" }]
+                                            }
+                                        }
+                                    ])
+                                }
+                            ]
+                        ]
+                    }
+                }
+            })
+        ]
+    },
+    resolve: {
+        // 尝试按顺序解析这些后缀名。如果有多个文件有相同的名字，但后缀名不同，webpack 会解析列在数组首位的后缀的文件 并跳过其余的后缀。
+        extensions: [".jsx", ".js", ".json", ".wasm"]
+    }
+}
